77a074d4ebabc6cc2b93cc7a348dcc5cc1b25336
Odysseas Samaras <odysseas.samaras@gmail.com>
2021-12-11 21:10:31 +0100

    exercise 2.3 - 2.5

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -63,4 +63,13 @@ object MyModule {
     }
     loop(0)
   }
+
+  def curry[A,B,C](f: (A, B) => C): A => B => C =
+    (a: A) => (b: B) => f(a, b)
+
+  def uncurry[A,B,C](f: A => B => C):(A, B) => C =
+    (a, b) => f(a)(b)
+
+  def compose[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }

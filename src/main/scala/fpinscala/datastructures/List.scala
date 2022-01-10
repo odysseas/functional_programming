4093f493b6805ed34d9dc6c852fd9e131bec71d3
Odysseas Samaras <odysseas.samaras@gmail.com>
2022-01-10 13:20:04 +0100

    exercise 3.12 done

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -37,25 +37,25 @@ object List {
 
   @tailrec
   def drop[A](l: List[A], n: Int): List[A] = {
-    if(n <= 0) l
+    if (n <= 0) l
     else l match {
       case Nil => Nil
-      case Cons(_, x) => drop(x, n-1)
+      case Cons(_, x) => drop(x, n - 1)
     }
   }
 
   @tailrec
   def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = {
     l match {
-      case Cons(x, xs) if f(x) => dropWhile(xs) (f)
+      case Cons(x, xs) if f(x) => dropWhile(xs)(f)
       case _ => l
     }
   }
 
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
-      case  Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Nil => a2
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
   def init[A](l: List[A]): List[A] =
@@ -68,14 +68,50 @@ object List {
   def init2[A](l: List[A]): List[A] = {
     import collection.mutable.ListBuffer
     val buf = new ListBuffer[A]
+
     @annotation.tailrec
     def go(cur: List[A]): List[A] = cur match {
       case Nil => sys.error("init empty list")
       case Cons(_, Nil) => List(buf.toList: _*)
       case Cons(h, t) => buf += h; go(t)
     }
+
     go(l)
   }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0)(_ * _)
+
+  def length[A](as: List[A]): Int =
+    foldRight(as, 0)((_, y) => 1 + y)
+
+  @tailrec
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+    as match {
+      case Nil => z
+      case Cons(h, t) => foldLeft(t, f(z,h))(f)
+    }
+
+  def sum3(ns: List[Int]): Int =
+    foldLeft(ns, 0)((x, y) => x + y)
+
+  def product3(ns: List[Double]): Double =
+    foldLeft(ns, 1.0)(_ * _)
+
+  def length2[A](l: List[A]): Int =
+    foldLeft(l, 0)((x, _) => x + 1)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, List[A]())((x, y) => Cons(y, x))
 
   def main(args: Array[String]): Unit = {
     val x = List(1, 2, 3, 4, 5) match {
--- src/test/scala/fpinscala/datastructures/ListTest.scala
+++ src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -10,4 +10,35 @@ class ListTest extends org.scalatest.funsuite.AnyFunSuite {
     assert(List.drop(l, 2) === List(3, 4))
     assert(List.drop(l, 5) === Nil)
   }
+  test("test length 1") {
+    val l = List(1)
+    assert(List.length(l) === 1)
+  }
+  test("test length 0") {
+    val l = List()
+    assert(List.length(l) === 0)
+  }
+  test("test length 3") {
+    val l = List(1, 2, 3)
+    assert(List.length(l) === 3)
+  }
+  test("test length 2") {
+    val l = List(0, 0)
+    assert(List.length(l) === 2)
+  }
+  test("reverse empty") {
+    val l = List()
+    val r = List.reverse(l)
+    assert(r === l)
+  }
+  test("reverse single") {
+    val l = List(1)
+    val r = List.reverse(l)
+    assert(r === l)
+  }
+  test("reverse multiple") {
+    val l = List(1, 2, 3)
+    val r = List.reverse(l)
+    assert(r === List(3, 2, 1))
+  }
 }

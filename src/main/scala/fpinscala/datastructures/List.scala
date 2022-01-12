0e06f947ed202479c1a760353ad61e5831dab9f5
Odysseas Samaras <odysseas.samaras@gmail.com>
2022-01-12 13:20:33 +0100

    exercise 3.23 done

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -1,6 +1,9 @@
 package fpinscala.datastructures
 
+import fpinscala.datastructures
+
 import scala.annotation.tailrec
+import scala.collection.mutable.ListBuffer
 
 sealed trait List[+A]
 
@@ -95,11 +98,26 @@ object List {
     foldRight(as, 0)((_, y) => 1 + y)
 
   @tailrec
-  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B =
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B =
     as match {
       case Nil => z
-      case Cons(h, t) => foldLeft(t, f(z,h))(f)
+      case Cons(h, t) => foldLeft(t, f(z, h))(f)
     }
+
+  def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(reverse(l), z)((b, a) => f(a, b))
+
+  def foldRightViaFoldLeft_1[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) => b)((g, a) => b => g(f(a, b)))(z)
+
+  def foldLeftViaFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(l, (b: B) => b)((a, g) => b => g(f(b, a)))(z)
+
+  def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] =
+    foldRight(l, r)(Cons(_, _))
+
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil: List[A])(append)
 
   def sum3(ns: List[Int]): Int =
     foldLeft(ns, 0)((x, y) => x + y)
@@ -112,6 +130,63 @@ object List {
 
   def reverse[A](l: List[A]): List[A] =
     foldLeft(l, List[A]())((x, y) => Cons(y, x))
+
+  def addOne(l: List[Int]): List[Int] =
+    foldRight(l, Nil: List[Int])((x, y) => Cons(x + 1, y))
+
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((x, y) => Cons(x.toString, y))
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldRight(as, Nil: List[B])((x, y) => Cons(f(x), y))
+
+  def map_1[A, B](as: List[A])(f: A => B): List[B] =
+    foldRightViaFoldLeft(as, Nil: List[B])((x, y) => Cons(f(x), y))
+
+  def map_2[A, B](as: List[A])(f: A => B): List[B] = {
+    val buf = new ListBuffer[B]
+
+    @tailrec
+    def go(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => buf += f(h); go(t)
+    }
+
+    go(as)
+    List(buf.toList: _*)
+  }
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+    val buf = new ListBuffer[A]
+
+    @tailrec
+    def go(l: List[A]): Unit = l match {
+      case Nil => ()
+      case Cons(h, t) => if (f(h)) buf += h; go(t)
+    }
+
+    go(as)
+    List(buf.toList: _*)
+  }
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] =
+    concat(map(as)(f))
+
+  def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] =
+    flatMap(as)(x => if(f(x)) List(x) else Nil)
+
+  def addPairWise(a: List[Int], b: List[Int]): List[Int] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairWise(t1, t2))
+  }
+
+  def zipWith[A](a: List[A], b: List[A])(f: (A, A) => A): List[A] = (a, b) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(h1, t1), Cons(h2, t2)) => Cons(f(h1, h2), zipWith(t1, t2)(f))
+  }
+
 
   def main(args: Array[String]): Unit = {
     val x = List(1, 2, 3, 4, 5) match {
--- src/test/scala/fpinscala/datastructures/ListTest.scala
+++ src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -41,4 +41,38 @@ class ListTest extends org.scalatest.funsuite.AnyFunSuite {
     val r = List.reverse(l)
     assert(r === List(3, 2, 1))
   }
+  test("test addOne empty") {
+    val l = Nil: List[Int]
+    assert(List.addOne(l) === List[Int]())
+  }
+  test("test addOne single") {
+    val l = List[Int](1)
+    assert(List.addOne(l) === List[Int](2))
+  }
+  test("test addOne multiple") {
+    val l = List[Int](1, 2, 3)
+    assert(List.addOne(l) === List[Int](2, 3, 4))
+  }
+  test("test filter multiple") {
+    val l = List[Int](1, 2, 3, 4)
+    assert(List.filter(l)(x => x % 2 == 0) === List[Int](2, 4))
+  }
+  test("test flatMap multiple") {
+    val l = List[Int](1, 2, 3)
+    assert(List.flatMap(l)(i => List(i, i)) === List[Int](1, 1, 2, 2, 3, 3))
+  }
+  test("test filter_2 multiple") {
+    val l = List[Int](1, 2, 3, 4)
+    assert(List.filterViaFlatMap(l)(x => x % 2 == 0) === List[Int](2, 4))
+  }
+  test("test addListElements multiple") {
+    val l1 = List[Int](1, 2, 3)
+    val l2 = List[Int](4, 5, 6)
+    assert(List.addPairWise(l1, l2) === List(5,7,9))
+  }
+  test("test zipWith multiple") {
+    val l1 = List[Int](1, 2, 3)
+    val l2 = List[Int](4, 5, 6)
+    assert(List.zipWith(l1, l2)((x, y) => x + y) === List(5,7,9))
+  }
 }
